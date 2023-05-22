081d8bb188d134c943c6d75416c5bfb06a7f90d4
Javier <JAVIERFOKJF11.21@ichat.sp.edu.sg>
2023-05-22 18:20:24 +0800

    first commit

---
+++ src/components/page-core-vitals.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Page Core Vitals</h1>
+            <p>Page Core Vitals</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/_app.jsx
@@ -0,0 +1,6 @@
+import PageCoreVitals from '../components/page-core-vitals';
+
+// eslint-disable-next-line no-unused-vars
+export default function App({ Component, pageProps }) {
+  return <PageCoreVitals />;
+}
